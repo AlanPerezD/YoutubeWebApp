2608460010d014699a148ef184e75ffa4b154e03
AlanPerezD <alanperezd@outlook.es>
2022-12-23 15:15:27 +0000

    fix coded, added return in app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Box sx={{ backgroundColor:"#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />}/>
+          <Route path="/video/:id" element={<VideoDetail />}/>
+          <Route path="/channel/:id" element={<ChannelDetail />}/>
+          <Route path="/search/:searchTerm" element={<SearchFeed />}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
