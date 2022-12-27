69bb3476b75e1b3aec5f3f8626fa051e8fb4dd09
AlanPerezD <52674692+AlanPerezD@users.noreply.github.com>
2022-12-27 19:54:58 -0300

    finished ChannelCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -11,7 +11,13 @@ return (
   <Box
     sx={{
       boxShadow: 'none',
-      borderRadius: '20px'
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center', 
+      width: { xs: '356px', md: '320px'},
+      height: '326px',
+      margin: 'auto'
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -19,8 +25,17 @@ return (
         <CardMedia
           image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
           alt={channelDetail?.snippet?.title}
-          sx={{ borderRadius: '50%', height: '180px', width: '180px'}}
-        /> 
+          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
+        />
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}
+          <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }}/>
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography>
+            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+          </Typography>
+        )}
       </CardContent>
     </Link>
   </Box>
