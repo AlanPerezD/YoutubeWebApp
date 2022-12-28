baee96eadb15da394b33fb4ca02d7c6cd5c17a35
AlanPerezD <52674692+AlanPerezD@users.noreply.github.com>
2022-12-28 07:16:13 -0300

    finished commit

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 
-const ChannelCard = ({ channelDetail}) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
 return (
   <Box
     sx={{
@@ -17,7 +17,8 @@ return (
       alignItems: 'center', 
       width: { xs: '356px', md: '320px'},
       height: '326px',
-      margin: 'auto'
+      margin: 'auto',
+      marginTop
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
